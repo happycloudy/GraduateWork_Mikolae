cff1054ef8cf508bd31a80afd5e87ddc396d5d28
happycloudy <arion.mikola@gmail.com>
2023-04-08 01:11:56 +0300

    fix student login

--- client/src/modules/Login/components/LoginStudent/LoginStudent.tsx
+++ client/src/modules/Login/components/LoginStudent/LoginStudent.tsx
@@ -2,15 +2,23 @@ import React, {useEffect} from 'react';
 import {useUserStore} from "../../../../stores/user/user.store";
 import {Link, useNavigate} from "react-router-dom";
 import {useLoginStudentMutation} from "../../../../services/users/users.service";
-import {Button, Form, Row} from "antd";
+import {Button, Form, notification, Row} from "antd";
 
 const LoginStudent = () => {
     const uuid = useUserStore(state => state.uuid)
     const setStudent = useUserStore(state => state.setStudent)
+    const [api, contextHolder] = notification.useNotification();
     const mutation = useLoginStudentMutation()
     const navigate = useNavigate()
 
+
     const handleSubmit = () => mutation.mutate(uuid)
+
+    const openNotificationWithIcon = (title: string, description: string = '') => api.error({
+        message: title,
+        description
+    });
+
 
     useEffect(() => {
         if (mutation.isSuccess) {
@@ -19,8 +27,24 @@ const LoginStudent = () => {
         }
     }, [mutation.isSuccess])
 
+    useEffect(() => {
+        if (mutation.isError) {
+            mutation?.error?.response?.json().then(res => {
+                switch (res.statusCode) {
+                    case(401):
+                        openNotificationWithIcon('Похоже, вы на этом устройстве заходите в первый раз, зарегистрируйтесь')
+                        break;
+                    default:
+
+                        break;
+                }
+            }) || openNotificationWithIcon('Ошибка соединения')
+        }
+    }, [mutation.isError])
+
     return (
         <Form onFinish={handleSubmit}>
+            {contextHolder}
             <Form.Item>
                 <Row justify={"center"}>
                     <Button htmlType={'submit'} size={'middle'}>
