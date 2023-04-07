6dfee50e8f8a9581cfeff1b448dcbfcdf541447c
happycloudy <arion.mikola@gmail.com>
2023-04-08 01:19:06 +0300

    add LastLessonCard.tsx

--- client/src/modules/InfoCards/components/LastLessonCard/LastLessonCard.tsx
+++ client/src/modules/InfoCards/components/LastLessonCard/LastLessonCard.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import {Card} from "antd";
+import {Button, Card, Row, Typography} from "antd";
 
 export const LastLessonCard = () => {
     return (
         <Card title={'Последний созданный ключ'} bordered={false} style={{width: 300}}>
-
+            <Row justify={"center"}>
+                <Typography.Title level={5}>Веб программирование</Typography.Title>
+                <Button>
+                    rfgeg234rge2
+                </Button>
+                <Typography.Text type={'secondary'}>нажми на кнопку, чтобы скопировать</Typography.Text>
+            </Row>
         </Card>
     );
 };
