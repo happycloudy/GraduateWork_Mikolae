6dfaaea4fe75ccdbcf40fd19b102472ba212e19a
happycloudy <arion.mikola@gmail.com>
2023-04-07 12:23:09 +0300

    add exit button

--- client/src/pages/teacherHome/TeacherHome.tsx
+++ client/src/pages/teacherHome/TeacherHome.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import PageLayout from "../../modules/Helpers/components/PageLayout/PageLayout";
+import {FloatButton} from "antd";
+import {LogoutOutlined} from "@ant-design/icons";
+import {useUserStore} from "../../stores/user/user.store";
+import {useNavigate} from "react-router-dom";
 
 const TeacherHome = () => {
+    const logout = useUserStore(state => state.logout)
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        logout()
+        navigate('/')
+    }
+
     return (
         <PageLayout>
-            Тут панель преподавателя
+            <FloatButton type={'primary'} icon={<LogoutOutlined />} onClick={handleLogout}/>
+
         </PageLayout>
     );
 };
