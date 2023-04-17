5f56ba2a7b55723e103373f5524b826bbe5f9447
happycloudy <arion.mikola@gmail.com>
2023-04-17 13:31:37 +0300

    fix parser

--- fetchMochData/main.js
+++ fetchMochData/main.js
@@ -2,13 +2,14 @@ const path = require('path')
 const xlsx = require('node-xlsx').default;
 const getGroupsByName = require('./helpers/getGroupsByName')
 const getLessons = require('./helpers/getLessons')
+const fs = require("fs");
 
 const input = path.resolve('./data/input_data_course_2.xlsx')
 
 const rawData = xlsx.parse(input);
 
 const groups = []
-const lessons = []
+let lessons = []
 
 // console.log(getLessons(rawData[0].data))
 
@@ -20,4 +21,25 @@ for (const list of rawData) {
   lessons.push(currentLessons)
 }
 
-console.log(lessons)
+lessons = lessons.flat()
+
+// console.log(lessons[1])
+
+const lessonsSet = new Set()
+
+lessons.forEach(lessonItem => {
+  lessonItem.lessons.forEach(item => {
+    lessonsSet.add(item)
+  })
+})
+
+
+// запись в groups
+
+let groupsJson = JSON.stringify({groups}, null, 2)
+fs.writeFileSync('groups.json', groupsJson)
+
+// запись в предметы
+
+let lessonsJson = JSON.stringify({lessons: Array.from(lessonsSet)}, null, 2)
+fs.writeFileSync('lessons.json', lessonsJson)
